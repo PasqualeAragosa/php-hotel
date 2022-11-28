2bf95835560398874e9b12c44b6238a4857cfaa6
Pasquale Aragosa <aragosap@gmail.com>
2022-11-28 17:04:24 +0100

    add structure

--- index.php
+++ index.php
@@ -55,24 +55,46 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+    </style>
 </head>
 
 <body>
 
-    <div class="wrapper">
+    <div class="wrapper my-5">
+        <h1 class="text-center my-5">Hotels</h1>
         <div class="container">
-            <div class="row row-cols-5">
-                <?php foreach ($hotels as $hotel) ?>
-                <div class="col">
-                    <ul>
-                        <li> Nome: <?php $hotel['name'] ?></li>
-                        <li> Descrizione: <?php $hotel['description'] ?></li>
-                        <li> Parcheggio: <?php $hotel['parking'] ?></li>
-                        <li> Voto: <?php $hotel['vote'] ?></li>
-                        <li> Distanza dal centro: <?php $hotel['distance_to_center'] ?></li>
-                    </ul>
-                </div>
-                <!-- /.col-4 -->
+            <div class="row row-cols-3 g-3">
+                <?php foreach ($hotels as $hotel) : ?>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?php echo $hotel['name'] ?>
+                                </h5>
+                                <p class="card-text">
+                                    <?php echo $hotel['description'] ?>
+                                </p>
+                                <p class="card-text">
+                                    Parcheggio:
+                                    <?php if ($hotel['parking']) {
+                                        echo 'Si';
+                                    } else {
+                                        echo 'No';
+                                    }
+                                    ?>
+                                </p>
+                                <p class="card-text">
+                                    Voto: <?php echo $hotel['vote'] ?>/5
+                                </p>
+                                <p class="card-text">
+                                    Distanza dal centro: <?php echo $hotel['distance_to_center'] ?> km
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.col-4 -->
+                <?php endforeach; ?>
             </div>
             <!-- /.row -->
         </div>
